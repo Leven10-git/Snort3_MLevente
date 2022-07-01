52111b60960f2ebbe85118c42ce7f9c7ccdcb9c4
root <root@demo.us-west4-b.c.alert-cursor-355016.internal>
2022-07-01 20:37:12 +0000

    added latest code

--- Snort3/snort-entrypoint.sh
+++ Snort3/snort-entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-exec /root/install/snort3/bin/snort  -c /root/install/snort3/etc/snort/snort_custom_lua/snort.lua $@ -l /var/log/snort --plugin-path /usr/local/snort/extra --daq-dir /usr/local/lib/daq/ -k none
-
+exec /root/install/snort3/bin/snort  -c /root/install/snort3/etc/snort/snort_custom_lua/snort.lua $@ -l /var/log/snort --plugin-path /usr/local/snort/extra --daq-dir /usr/local/lib/daq/ -k none -j 7000
